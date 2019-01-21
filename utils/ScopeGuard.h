7cb045bf0ff941fb40906b1c1cd04497d6f7a91c
Teng Yiliang <ylteng@outlook.com>
2019-01-21 17:31:22 +0800

    update ScopeGuard, remove type deduce for ctor.
    Signed-off-by: Teng Yiliang <ylteng@outlook.com>

--- utils/ScopeGuard.h
+++ utils/ScopeGuard.h
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <functional>
+#include <iostream>
 using namespace std;
 
 template< typename Lambda >
@@ -12,36 +13,45 @@ class ScopeGuard
 
     public:
     ScopeGuard() = delete;
-    template <typename L> ScopeGuard(const ScopeGuard<L>&) = delete;
-    template <typename L> void operator=(const ScopeGuard<L>&) = delete;
-    template <typename L> void operator=(ScopeGuard<L>&&) = delete;
+    /*
+    ScopeGuard(const ScopeGuard<Lambda>&) = delete;
+    void operator=(const ScopeGuard<Lambda>&) = delete;
+    void operator=(ScopeGuard<Lambda>&&) = delete;
+    */
+    ScopeGuard(const ScopeGuard&) = delete;
+    void operator=(const ScopeGuard&) = delete;
+    void operator=(ScopeGuard&&) = delete;
 
-    // make sure this is not a copy ctor
-    template <typename L>
-    explicit ScopeGuard(L&& _l):
+    // is_nothrow_constructible: identifies whether T is a constructible type using the set of argument
+    // types specified by Arg, and such construction is known not to throw any exception.
+    explicit ScopeGuard(Lambda&& _l) noexcept(std::is_nothrow_constructible<Lambda, Lambda&&>::value) :
         dismissed(false),
-        rollbackLambda(std::forward<L>(_l)) // avoid copying unless necessary
+        rollbackLambda(std::forward<Lambda>(_l)) // avoid copying unless necessary
     {}
 
     // move constructor
-    ScopeGuard(ScopeGuard&& that):
+    ScopeGuard(ScopeGuard&& that) noexcept(std::is_nothrow_constructible<Lambda, Lambda&&>::value) :
         dismissed(that.dismissed),
-        rollbackLambda(std::move(that.rollbackLambda)) {
-            that.dismissed = true;
-        }
-
-    ~ScopeGuard()
+        rollbackLambda(std::forward<Lambda>(that.rollbackLambda))
     {
-        if (!dismissed)
-            rollbackLambda(); // what if this throws?
+        that.dismissed = true;
+    }
+
+    ~ScopeGuard() noexcept
+    {
+        if (!dismissed){
+            try{
+                rollbackLambda(); // what if this throws?
+            }catch(...){cerr<<__FUNCTION__<<" throw exception!"<<endl;}
+        }
     }
     void dismiss() { dismissed = true; } // no need for const
 };
 
 template<typename rLambda>
-ScopeGuard< rLambda > makeScopeGuard(rLambda&& _r)
+ScopeGuard<rLambda> makeScopeGuard(rLambda&& _r)
 {
-    return ScopeGuard< rLambda >( std::forward<rLambda>( _r ));
+    return ScopeGuard<rLambda>( std::forward<rLambda>( _r ));
 }
 
 #define DO_STRING_JOIN2(arg1, arg2) arg1 ## arg2
